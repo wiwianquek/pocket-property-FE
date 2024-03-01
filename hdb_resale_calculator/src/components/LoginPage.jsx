d7d63ca4eed9c9200eb3b0545db212f9efac8206
wiwianquek <136752154+wiwianquek@users.noreply.github.com>
2024-03-01 23:36:02 +0800

    adjusted UI for login and signup page

--- hdb_resale_calculator/src/components/LoginPage.jsx
+++ hdb_resale_calculator/src/components/LoginPage.jsx
@@ -1,164 +1,10 @@
-// import React, { useState } from 'react';
-// import { Box, Flex, Input, Button, FormControl, FormLabel, Heading, useToast, Spinner } from '@chakra-ui/react';
-// import { getLoginDetails, loginUser } from '../service/users'; 
-// import { useNavigate } from 'react-router-dom';
-// import { hashDataWithSaltRounds, storeToken } from '../util/security'; 
-// import { Link } from 'react-router-dom';
-// import pocketPropertyImage from '../assets/pocketproperty.png';
-  
-// export default function LoginPage() {
-//     const [formData, setFormData] = useState({
-//       email: '',
-//       password: '',
-//     });
-//     const [loading, setLoading] = useState(false); 
-  
-//     const toast = useToast();
-//     const navigate = useNavigate();
-  
-//     const handleChange = (e) => {
-//       setFormData({ ...formData, [e.target.name]: e.target.value });
-//     };
-      
-//     const handleSubmit = async (e) => {
-//       e.preventDefault();
-//       setLoading(true); // Set loading to true
-//       try {
-//         const loginDetails = await getLoginDetails(formData.email);
-//         const hashedPassword = hashDataWithSaltRounds(
-//           formData.password,
-//           loginDetails.salt,
-//           loginDetails.iterations
-//         );
-//       // Get the salt and iterations from the backend for the user's email
-//       try {
-//         const loginDetails = await getLoginDetails(formData.email);
-//         const hashedPassword = hashDataWithSaltRounds(
-//           formData.password,
-//           loginDetails.salt,
-//           loginDetails.iterations
-//         );
-  
-//         // Attempt to log in with the hashed password
-//         const token = await loginUser({ email: formData.email, password: hashedPassword });
-//         storeToken(token); // Store the token
-//         toast({
-//           title: 'Login successful.',
-//           description: "You're now logged in!",
-//           status: 'success',
-//           duration: 5000,
-//           isClosable: true,
-//         });
-//         navigate('/'); // Navigate to the dashboard
-//       } catch (error) {
-//         console.error(error);
-//         toast({
-//           title: 'Login Failed.',
-//           description: 'The login details are incorrect.',
-//           status: 'error',
-//           duration: 5000,
-//           isClosable: true,
-//         });
-//         navigate('/');
-//       } catch (error) {
-//         console.error(error);
-//         toast({
-//           title: 'Login Failed.',
-//           description: 'The login details are incorrect.',
-//           status: 'error',
-//           duration: 5000,
-//           isClosable: true,
-//         });
-//       } finally {
-//         setLoading(false); // Set loading to false regardless of the outcome
-//       }
-//     };
-  
-//   return (
-    // <Flex
-    //   minH="100vh"
-    //   w="full"
-    //   align="center"
-    //   justify="flex-end"
-    //   bgImage={`url(${pocketPropertyImage})`}
-    //   bgSize="212vh"
-    //   bgPosition="center"
-    //   position="relative"
-    //   pr={{ base: 4, md: 8, lg: 12 }}
-    // >
-//       <Box
-//         p={8}
-//         borderRadius="md"
-//         boxShadow="0px 4px 8px rgba(0, 0, 0, 0.1)"
-//         border="1px solid transparent"
-//         bg="white"
-//         maxW="md"
-//         width="full"
-//         zIndex="docked"
-//         mr="20"
-//         _hover={{ transform: 'translateY(-5px)', boxShadow: 'lg' }}
-//         transition="transform 0.3s ease"
-//       >
-//         <form onSubmit={handleSubmit}>
-//           <Heading as="h2" size="xl" mb="6" textAlign="center">Log In</Heading>
-//           <FormControl mb="4">
-//             <FormLabel htmlFor="email">Email</FormLabel>
-//             <Input
-//               id="email"
-//               type="email"
-//               name="email"
-//               value={formData.email}
-//               onChange={handleChange}
-//               required
-//             />
-//           </FormControl>
-//           <FormControl mb="4">
-//             <FormLabel htmlFor="password">Password</FormLabel>
-//             <Input
-//               id="password"
-//               type="password"
-//               name="password"
-//               value={formData.password}
-//               onChange={handleChange}
-//               required
-//             />
-//           </FormControl>
-//           <Button
-//             type="submit"
-//             colorScheme="blue"
-//             size="lg"
-//             mt="4"
-//             w="full"
-//             isLoading={loading} // Add this line
-//             loadingText="Logging in" // Add this line
-//             spinner={<Spinner size="sm" />} // Optional: Customize the spinner
-//             disabled={loading} // Add this line
-//           >
-//             Log In
-//           </Button>
-//         </form>
-//         <Button
-//             as={Link} // This makes the button act as a router link
-//             to="/signup" // Path to the signup page
-//             colorScheme="blue"
-//             variant="link" // Make it look like a link
-//             mt="4"
-//             >
-//             Don't have an account? Sign up here.
-//             </Button>
-//       </Box>
-//     </Flex>
-//   );
-// }
-
-
 import React, { useState } from 'react';
-import { Box, Flex, Input, Button, FormControl, FormLabel, Heading, useToast, Spinner } from '@chakra-ui/react';
+import { Box, Flex, Grid, GridItem, Input, Button, FormControl, FormLabel, Heading, useToast, Spinner } from '@chakra-ui/react';
 import { getLoginDetails, loginUser } from '../service/users'; 
 import { useNavigate } from 'react-router-dom';
 import { hashDataWithSaltRounds, storeToken } from '../util/security'; 
 import { Link } from 'react-router-dom';
-import pocketPropertyImage from '../assets/pocketproperty.png';
+import pocketPropertyImage from '../assets/hdbhome.png';
   
 export default function LoginPage() {
     const [formData, setFormData] = useState({
@@ -210,78 +56,88 @@ export default function LoginPage() {
     };
   
     return (
-      <Flex
-      minH="100vh"
-      w="full"
-      align="center"
-      justify="flex-end" 
-      bgImage={`url(${pocketPropertyImage})`}
-      bgSize="212vh"
-      bgPosition="center"
-      position="relative"
-      pr={{ base: 4, md: 8, lg: 12 }} 
-    >
-        <Box
-          p={3}
-          borderRadius="md"
-          boxShadow="0px 4px 8px rgba(0, 0, 0, 0.1)"
-          border="1px solid transparent"
-          bg="white"
-          maxW="md" 
-          width="full" 
-          zIndex="docked"
-          mr="20"
-          _hover={{ transform: 'translateY(-5px)', boxShadow: 'lg' }}
-          transition="transform 0.3s ease"
-        >
-          <form onSubmit={handleSubmit}>
-            <Heading as="h2" size="xl" mb="6" textAlign="center">Log In</Heading>
-            <FormControl mb="4">
-              <FormLabel htmlFor="email">Email</FormLabel>
-              <Input
-                id="email"
-                type="email"
-                name="email"
-                value={formData.email}
-                onChange={handleChange}
-                required
-              />
-            </FormControl>
-            <FormControl mb="4">
-              <FormLabel htmlFor="password">Password</FormLabel>
-              <Input
-                id="password"
-                type="password"
-                name="password"
-                value={formData.password}
-                onChange={handleChange}
-                required
-              />
-            </FormControl>
-            <Button
-              type="submit"
-              colorScheme="blue"
-              size="lg"
-              mt="4"
+      <Grid
+        h="100vh"
+        templateColumns={{ md: "1.5fr 1fr" }} // Split into two columns on medium screens and above
+        templateRows="1fr"
+      >
+        <GridItem
+          bgImage={`url(${pocketPropertyImage})`}
+          bgSize="cover"
+          bgPosition="center"
+        />
+        <GridItem>
+          <Flex
+            align="center"
+            justify="center"
+            p={{ base: 4, md: 8, lg: 12 }}
+          >
+            <Box
+              p={3}
+              mt={{ base: "50%", md: "30%" }}
+              borderRadius="md"
+              boxShadow="0px 4px 8px rgba(0, 0, 0, 0.1)"
+              border="1px solid transparent"
+              bg="white"
+              maxW="md"
               w="full"
-              isLoading={loading} // Add this line
-              loadingText="Logging in" // Add this line
-              spinner={<Spinner size="sm" />} // Optional: Customize the spinner
-              disabled={loading} // Add this line
+              zIndex="docked"
+              _hover={{ transform: 'translateY(-5px)', boxShadow: 'lg' }}
+              transition="transform 0.3s ease"
             >
-              Log In
-            </Button>
-          </form>
-          <Button
-              as={Link} // This makes the button act as a router link
-              to="/signup" // Path to the signup page
-              colorScheme="blue"
-              variant="link" // Make it look like a link
-              mt="4"
-              >
-              Don't have an account? Sign up here.
-              </Button>
-        </Box>
-      </Flex>
+               <form onSubmit={handleSubmit}>
+                <Heading as="h2" size="xl" mb="6" textAlign="center">Log In</Heading>
+                <FormControl mb="4">
+                  <FormLabel htmlFor="email">Email</FormLabel>
+                  <Input
+                    id="email"
+                    type="email"
+                    name="email"
+                    value={formData.email}
+                    onChange={handleChange}
+                    required
+                  />
+                </FormControl>
+                <FormControl mb="4">
+                  <FormLabel htmlFor="password">Password</FormLabel>
+                  <Input
+                    id="password"
+                    type="password"
+                    name="password"
+                    value={formData.password}
+                    onChange={handleChange}
+                    required
+                  />
+                </FormControl>
+                <Button
+                  type="submit"
+                  colorScheme="blue"
+                  size="lg"
+                  mt="4"
+                  w="full"
+                  isLoading={loading} // Add this line
+                  loadingText="Logging in" // Add this line
+                  spinner={<Spinner size="sm" />} // Optional: Customize the spinner
+                  disabled={loading} // Add this line
+                >
+                  Log In
+                </Button>
+              </form>
+              <Button
+                  as={Link} // This makes the button act as a router link
+                  to="/signup" // Path to the signup page
+                  colorScheme="blue"
+                  variant="link" // Make it look like a link
+                  mt="4"
+                  >
+                  Don't have an account? Sign up here.
+                  </Button>
+            </Box>
+          </Flex>
+        </GridItem>
+      </Grid>
     );
 }
+
+
+
--- hdb_resale_calculator/src/components/SignUpPage.jsx
+++ hdb_resale_calculator/src/components/SignUpPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import { Box, Flex, Input, Button, FormControl, FormLabel, Heading, useToast } from '@chakra-ui/react';
-import pocketPropertyImage from '../assets/pocketproperty.png'; // Make sure this path is correct
+import { Box, Flex, Grid, GridItem, Input, Button, FormControl, FormLabel, Heading, useToast } from '@chakra-ui/react';
+import pocketPropertyImage from '../assets/hdbhome.png'; // Make sure this path is correct
 import { signUp } from '../service/users'; // Make sure to import the signUp function
 import { hashData } from '../util/security'; // Importing hashData function from your utilities
 import { Link } from 'react-router-dom';
@@ -56,110 +56,120 @@ export default function SignUpPage() {
   };
 
   return (
-    <Flex
-      minH="100vh"
-      w="full"
-      align="center"
-      justify="flex-end" 
-      bgImage={`url(${pocketPropertyImage})`}
-      bgSize="212vh"
-      bgPosition="center"
-      position="relative"
-      pr={{ base: 4, md: 8, lg: 12 }} 
+    <Grid
+      h="100vh"
+      templateColumns={{ md: "1.5fr 1fr" }} // Split into two columns on medium screens and above
+      templateRows="1fr"
     >
-        <Box
-          p={3}
-          borderRadius="md"
-          boxShadow="0px 4px 8px rgba(0, 0, 0, 0.1)"
-          border="1px solid transparent"
-          bg="white"
-          maxW="md" 
-          width="full" 
-          zIndex="docked"
-          mr="20"
-          _hover={{ transform: 'translateY(-5px)', boxShadow: 'lg' }}
-          transition="transform 0.3s ease"
+      <GridItem
+        bgImage={`url(${pocketPropertyImage})`}
+        bgSize="cover"
+        bgPosition="center"
+      />
+      <GridItem>
+        <Flex
+          align="center"
+          justify="center"
+          p={{ base: 4, md: 8, lg: 12 }}
         >
-          
-          <form onSubmit={handleSubmit}>
-            <Heading as="h2" size="xl" mb="6" textAlign="center">Sign Up</Heading>
-            <FormControl mb="4">
-              <FormLabel htmlFor="First Name">First Name</FormLabel>
-              <Input
-                id="first_name"
-                type="text"
-                name="first_name"
-                value={formData.name}
-                onChange={handleChange}
-                required
-              />
-            </FormControl>
-            <FormControl mb="4">
-              <FormLabel htmlFor="Last Name">Last Name</FormLabel>
-              <Input
-                id="last_name"
-                type="text"
-                name="last_name"
-                value={formData.name}
-                onChange={handleChange}
-                required
-              />
-            </FormControl>
-            <FormControl mb="4">
-              <FormLabel htmlFor="username">Username</FormLabel>
-              <Input
-                id="username"
-                type="text"
-                name="username"
-                value={formData.username}
-                onChange={handleChange}
-                required
-              />
-            </FormControl>
-            <FormControl mb="4">
-              <FormLabel htmlFor="email">Email</FormLabel>
-              <Input
-                id="email"
-                type="email"
-                name="email"
-                value={formData.email}
-                onChange={handleChange}
-                required
-              />
-            </FormControl>
-            <FormControl mb="4">
-              <FormLabel htmlFor="password">Password</FormLabel>
-              <Input
-                id="password"
-                type="password"
-                name="password"
-                value={formData.password}
-                onChange={handleChange}
-                required
-              />
-            </FormControl>
+          <Box
+            p={3}
+            mt={{ base: "50%", md: "10%" }}
+            borderRadius="md"
+            boxShadow="0px 4px 8px rgba(0, 0, 0, 0.1)"
+            border="1px solid transparent"
+            bg="white"
+            maxW="md"
+            w="full"
+            zIndex="docked"
+            _hover={{ transform: 'translateY(-5px)', boxShadow: 'lg' }}
+            transition="transform 0.3s ease"
+          >
+              <form onSubmit={handleSubmit}>
+              <Heading as="h2" size="xl" mb="6" textAlign="center">Sign Up</Heading>
+              <FormControl mb="4">
+                <FormLabel htmlFor="First Name">First Name</FormLabel>
+                <Input
+                  id="first_name"
+                  type="text"
+                  name="first_name"
+                  value={formData.name}
+                  onChange={handleChange}
+                  required
+                />
+              </FormControl>
+              <FormControl mb="4">
+                <FormLabel htmlFor="Last Name">Last Name</FormLabel>
+                <Input
+                  id="last_name"
+                  type="text"
+                  name="last_name"
+                  value={formData.name}
+                  onChange={handleChange}
+                  required
+                />
+              </FormControl>
+              <FormControl mb="4">
+                <FormLabel htmlFor="username">Username</FormLabel>
+                <Input
+                  id="username"
+                  type="text"
+                  name="username"
+                  value={formData.username}
+                  onChange={handleChange}
+                  required
+                />
+              </FormControl>
+              <FormControl mb="4">
+                <FormLabel htmlFor="email">Email</FormLabel>
+                <Input
+                  id="email"
+                  type="email"
+                  name="email"
+                  value={formData.email}
+                  onChange={handleChange}
+                  required
+                />
+              </FormControl>
+              <FormControl mb="4">
+                <FormLabel htmlFor="password">Password</FormLabel>
+                <Input
+                  id="password"
+                  type="password"
+                  name="password"
+                  value={formData.password}
+                  onChange={handleChange}
+                  required
+                />
+              </FormControl>
+              <Button
+                type="submit"
+                colorScheme="blue"
+                size="lg"
+                mt="4"
+                w="full"
+              >
+                Sign Up
+              </Button>
+            </form>
             <Button
-              type="submit"
+              as={Link} 
+              to="/login" 
               colorScheme="blue"
-              size="lg"
+              variant="link" 
               mt="4"
-              w="full"
-            >
-              Sign Up
-            </Button>
-          </form>
-          <Button
-            as={Link} 
-            to="/login" 
-            colorScheme="blue"
-            variant="link" 
-            mt="4"
-            >
-            Have an account? Login here.
-            </Button>
-        </Box>
-    </Flex>
+              >
+              Have an account? Login here.
+              </Button>
+          </Box>
+        </Flex>
+      </GridItem>
+    </Grid>
   );
 }
+
+
+
+
 
 
