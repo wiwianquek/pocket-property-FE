ed0ccdfd83c3a4bbeb6fd71342b6c9a4d984943d
wiwianquek <136752154+wiwianquek@users.noreply.github.com>
2024-03-02 11:51:06 +0800

    added logo on headers

--- hdb_resale_calculator/src/components/LoginPage.jsx
+++ hdb_resale_calculator/src/components/LoginPage.jsx
@@ -56,88 +56,94 @@ export default function LoginPage() {
     };
   
     return (
-      <Grid
-        h="100vh"
-        templateColumns={{ md: "1.5fr 1fr" }} // Split into two columns on medium screens and above
-        templateRows="1fr"
-      >
-        <GridItem
-          bgImage={`url(${pocketPropertyImage})`}
-          bgSize="cover"
-          bgPosition="center"
-        />
-        <GridItem>
-          <Flex
-            align="center"
-            justify="center"
-            p={{ base: 4, md: 8, lg: 12 }}
-          >
-            <Box
-              p={3}
-              mt={{ base: "50%", md: "30%" }}
-              borderRadius="md"
-              boxShadow="0px 4px 8px rgba(0, 0, 0, 0.1)"
-              border="1px solid transparent"
-              bg="white"
-              maxW="md"
-              w="full"
-              zIndex="docked"
-              _hover={{ transform: 'translateY(-5px)', boxShadow: 'lg' }}
-              transition="transform 0.3s ease"
+      <>
+        <Flex bg="gray.100" as="header" w="full" justify="space-between" p={4} align="center" boxShadow="sm">
+          <Heading as="h1" size="lg" ml={8}>
+            Pocket Property
+          </Heading>
+          {/* Additional elements for the header can be added here */}
+        </Flex>
+    
+        <Grid
+          h="100vh"
+          templateColumns={{ md: "1.5fr 1fr" }} // Split into two columns on medium screens and above
+          templateRows="1fr"
+        >
+          <GridItem
+            bgImage={`url(${pocketPropertyImage})`}
+            bgSize="cover"
+            bgPosition="center"
+          />
+          <GridItem>
+            <Flex
+              align="center"
+              justify="center"
+              p={{ base: 4, md: 8, lg: 12 }}
             >
-               <form onSubmit={handleSubmit}>
-                <Heading as="h2" size="xl" mb="6" textAlign="center">Log In</Heading>
-                <FormControl mb="4">
-                  <FormLabel htmlFor="email">Email</FormLabel>
-                  <Input
-                    id="email"
-                    type="email"
-                    name="email"
-                    value={formData.email}
-                    onChange={handleChange}
-                    required
-                  />
-                </FormControl>
-                <FormControl mb="4">
-                  <FormLabel htmlFor="password">Password</FormLabel>
-                  <Input
-                    id="password"
-                    type="password"
-                    name="password"
-                    value={formData.password}
-                    onChange={handleChange}
-                    required
-                  />
-                </FormControl>
-                <Button
-                  type="submit"
-                  colorScheme="blue"
-                  size="lg"
-                  mt="4"
-                  w="full"
-                  isLoading={loading} // Add this line
-                  loadingText="Logging in" // Add this line
-                  spinner={<Spinner size="sm" />} // Optional: Customize the spinner
-                  disabled={loading} // Add this line
-                >
-                  Log In
-                </Button>
-              </form>
-              <Button
-                  as={Link} // This makes the button act as a router link
-                  to="/signup" // Path to the signup page
-                  colorScheme="blue"
-                  variant="link" // Make it look like a link
-                  mt="4"
+              <Box
+                p={3}
+                mt={{ base: "50%", md: "30%" }}
+                borderRadius="md"
+                boxShadow="0px 4px 8px rgba(0, 0, 0, 0.1)"
+                border="1px solid transparent"
+                bg="white"
+                maxW="md"
+                w="full"
+                zIndex="docked"
+                _hover={{ transform: 'translateY(-5px)', boxShadow: 'lg' }}
+                transition="transform 0.3s ease"
+              >
+                <form onSubmit={handleSubmit}>
+                  <Heading as="h2" size="xl" mb="6" textAlign="center">Log In</Heading>
+                  <FormControl mb="4">
+                    <FormLabel htmlFor="email">Email</FormLabel>
+                    <Input
+                      id="email"
+                      type="email"
+                      name="email"
+                      value={formData.email}
+                      onChange={handleChange}
+                      required
+                    />
+                  </FormControl>
+                  <FormControl mb="4">
+                    <FormLabel htmlFor="password">Password</FormLabel>
+                    <Input
+                      id="password"
+                      type="password"
+                      name="password"
+                      value={formData.password}
+                      onChange={handleChange}
+                      required
+                    />
+                  </FormControl>
+                  <Button
+                    type="submit"
+                    colorScheme="blue"
+                    size="lg"
+                    mt="4"
+                    w="full"
+                    isLoading={loading}
+                    loadingText="Logging in"
+                    disabled={loading}
                   >
-                  Don't have an account? Sign up here.
+                    Log In
                   </Button>
-            </Box>
-          </Flex>
-        </GridItem>
-      </Grid>
-    );
+                </form>
+                <Button
+                  as={Link}
+                  to="/signup"
+                  colorScheme="blue"
+                  variant="link"
+                  mt="4"
+                >
+                  Don't have an account? Sign up here.
+                </Button>
+              </Box>
+            </Flex>
+          </GridItem>
+        </Grid>
+      </>
+    );    
 }
-
-
 
--- hdb_resale_calculator/src/components/SignUpPage.jsx
+++ hdb_resale_calculator/src/components/SignUpPage.jsx
@@ -56,120 +56,127 @@ export default function SignUpPage() {
   };
 
   return (
-    <Grid
-      h="100vh"
-      templateColumns={{ md: "1.5fr 1fr" }} // Split into two columns on medium screens and above
-      templateRows="1fr"
-    >
-      <GridItem
-        bgImage={`url(${pocketPropertyImage})`}
-        bgSize="cover"
-        bgPosition="center"
-      />
-      <GridItem>
-        <Flex
-          align="center"
-          justify="center"
-          p={{ base: 4, md: 8, lg: 12 }}
-        >
-          <Box
-            p={3}
-            mt={{ base: "50%", md: "10%" }}
-            borderRadius="md"
-            boxShadow="0px 4px 8px rgba(0, 0, 0, 0.1)"
-            border="1px solid transparent"
-            bg="white"
-            maxW="md"
-            w="full"
-            zIndex="docked"
-            _hover={{ transform: 'translateY(-5px)', boxShadow: 'lg' }}
-            transition="transform 0.3s ease"
+    <>
+      {/* Header Section */}
+      <Flex bg="gray.100" as="header" w="full" justify="space-between" p={4} align="center" boxShadow="sm">
+        <Heading as="h1" size="lg" ml={8}>
+          Pocket Property
+        </Heading>
+      </Flex>
+
+      {/* Grid Layout */}
+      <Grid
+        h="100vh"
+        templateColumns={{ md: "1.5fr 1fr" }}
+        templateRows="1fr"
+      >
+        <GridItem
+          bgImage={`url(${pocketPropertyImage})`}
+          bgSize="cover"
+          bgPosition="center"
+        />
+        <GridItem>
+          <Flex
+            align="center"
+            justify="center"
+            p={{ base: 4, md: 8, lg: 12 }}
           >
+            <Box
+              p={3}
+              mt={{ base: "50%", md: "10%" }}
+              borderRadius="md"
+              boxShadow="0px 4px 8px rgba(0, 0, 0, 0.1)"
+              border="1px solid transparent"
+              bg="white"
+              maxW="md"
+              w="full"
+              zIndex="docked"
+              _hover={{ transform: 'translateY(-5px)', boxShadow: 'lg' }}
+              transition="transform 0.3s ease"
+            >
+              {/* Form for Sign Up */}
               <form onSubmit={handleSubmit}>
-              <Heading as="h2" size="xl" mb="6" textAlign="center">Sign Up</Heading>
-              <FormControl mb="4">
-                <FormLabel htmlFor="First Name">First Name</FormLabel>
-                <Input
-                  id="first_name"
-                  type="text"
-                  name="first_name"
-                  value={formData.name}
-                  onChange={handleChange}
-                  required
-                />
-              </FormControl>
-              <FormControl mb="4">
-                <FormLabel htmlFor="Last Name">Last Name</FormLabel>
-                <Input
-                  id="last_name"
-                  type="text"
-                  name="last_name"
-                  value={formData.name}
-                  onChange={handleChange}
-                  required
-                />
-              </FormControl>
-              <FormControl mb="4">
-                <FormLabel htmlFor="username">Username</FormLabel>
-                <Input
-                  id="username"
-                  type="text"
-                  name="username"
-                  value={formData.username}
-                  onChange={handleChange}
-                  required
-                />
-              </FormControl>
-              <FormControl mb="4">
-                <FormLabel htmlFor="email">Email</FormLabel>
-                <Input
-                  id="email"
-                  type="email"
-                  name="email"
-                  value={formData.email}
-                  onChange={handleChange}
-                  required
-                />
-              </FormControl>
-              <FormControl mb="4">
-                <FormLabel htmlFor="password">Password</FormLabel>
-                <Input
-                  id="password"
-                  type="password"
-                  name="password"
-                  value={formData.password}
-                  onChange={handleChange}
-                  required
-                />
-              </FormControl>
+                <Heading as="h2" size="xl" mb="6" textAlign="center">Sign Up</Heading>
+                  <FormControl mb="4">
+                    <FormLabel htmlFor="First Name">First Name</FormLabel>
+                    <Input
+                      id="first_name"
+                      type="text"
+                      name="first_name"
+                      value={formData.name}
+                      onChange={handleChange}
+                      required
+                    />
+                  </FormControl>
+                  <FormControl mb="4">
+                    <FormLabel htmlFor="Last Name">Last Name</FormLabel>
+                    <Input
+                      id="last_name"
+                      type="text"
+                      name="last_name"
+                      value={formData.name}
+                      onChange={handleChange}
+                      required
+                    />
+                  </FormControl>
+                  <FormControl mb="4">
+                    <FormLabel htmlFor="username">Username</FormLabel>
+                    <Input
+                      id="username"
+                      type="text"
+                      name="username"
+                      value={formData.username}
+                      onChange={handleChange}
+                      required
+                    />
+                  </FormControl>
+                  <FormControl mb="4">
+                    <FormLabel htmlFor="email">Email</FormLabel>
+                    <Input
+                      id="email"
+                      type="email"
+                      name="email"
+                      value={formData.email}
+                      onChange={handleChange}
+                      required
+                    />
+                  </FormControl>
+                  <FormControl mb="4">
+                    <FormLabel htmlFor="password">Password</FormLabel>
+                    <Input
+                      id="password"
+                      type="password"
+                      name="password"
+                      value={formData.password}
+                      onChange={handleChange}
+                      required
+                    />
+                  </FormControl>
+                <Button
+                  type="submit"
+                  colorScheme="blue"
+                  size="lg"
+                  mt="4"
+                  w="full"
+                >
+                  Sign Up
+                </Button>
+              </form>
+
+              {/* Link to Login Page */}
               <Button
-                type="submit"
+                as={Link} 
+                to="/login" 
                 colorScheme="blue"
-                size="lg"
+                variant="link" 
                 mt="4"
-                w="full"
               >
-                Sign Up
+                Have an account? Login here.
               </Button>
-            </form>
-            <Button
-              as={Link} 
-              to="/login" 
-              colorScheme="blue"
-              variant="link" 
-              mt="4"
-              >
-              Have an account? Login here.
-              </Button>
-          </Box>
-        </Flex>
-      </GridItem>
-    </Grid>
+            </Box>
+          </Flex>
+        </GridItem>
+      </Grid>
+    </>
   );
 }
-
-
-
-
-
-
